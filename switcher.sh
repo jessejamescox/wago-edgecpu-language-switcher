8c1071e58b4d45fd80abfa39fe269ca9f5ec46dc
Jesse Cox <jess.coxpdx@gmail.com>
2021-05-04 07:39:37 -0700

    Update switcher.sh

--- switcher.sh
+++ switcher.sh
@@ -2,11 +2,11 @@
 LANG="en_US.UTF-8 UTF-8"
 
 # after the repo has been cloned run this script
-mv ./keyboard /etc/default/keyboard
+mv ./wago-edgecpu-language-switcher/keyboard /etc/default/keyboard
 dpkg-reconfigure keyboard-configuration
 
 #move the locales file 
-mv ./locale.gen /etc/locale.gen
+mv ./wago-edgecpu-language-switcher/locale.gen /etc/locale.gen
 /usr/sbin/locale-gen
 . /etc/default/locale
 
